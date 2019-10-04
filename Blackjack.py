19b06a0cd2366d957669d1315ba7ce6ab6c1e353
hardik11794@gmail.com <hardik11794@gmail.com>
2019-10-03 22:37:03 -0400

    Game Structure Building

--- Blackjack.py
+++ Blackjack.py
@@ -1,6 +1,7 @@
 from Modules import Hand
 from Modules import Deck
 from Modules import Cards
+from Modules import Chips
 
 
 playing = True
@@ -79,4 +80,114 @@ def show_all(player,dealer):
     for card in player.cards:
         print(card)
     print(player.value)
+
+
+#*******************************************************************************************************
+#      This function takes hits
+#******************************************************************************************************* 
+
+def hit(deck,hand):
+    add_card(deck.deal())
+    Hand.adjust_for_aces    
+
+
+#*****************************************************************************************************
+#      This function print player or dealer won or lose 
+#*****************************************************************************************************
+
+def player_busts(player,dealer,Chips):
+    print("Player Busts!!!")
+    chips.lose_bet()
+
+def player_wins(player,dealer,Chips):
+    print("Player Won!!!")
+    chips.win_bet()
+
+def dealer_busts(player,dealer,Chips):
+    print("Dealer Busts!!!")
+    chips.lose_bet()
+
+def dealer_wins(player,dealer,Chips):
+    print("Delaer Busts!!!")
+    chips.win_bet()
+
+def push():
+    print("Dealer and Player tie! It's a push.")
+
+
+#*****************************************************************************************************
+#      This function ask for bet to the player
+#*****************************************************************************************************
+
+def take_bet(Chips):
+
+    while True:
+            try:
+                Chips.bet = int(input("Please, Enter your bet : "))
+            except:
+                print("Enter interger numbers only.")
+            else:
+                if Chips.bet > Chips.total:
+                    Print(f"Bet must be Less then availble balance {Chips.total}. ")
+                    continue
+                else:
+                    break
+   
+ #*********************************************************************************************************
+ #                   Game Arrangements
+ #*********************************************************************************************************
+
+while True:
+    # Print an opening statement
+    print("Welcome to BlackJack !!")
+
     
+    # Create & shuffle the deck, deal two cards to each player
+   
+    
+    Deck = Deck.Deck()
+    Deck.shuffle()
+
+    Players_Hand = Hand.Hand()
+    Players_Hand.add_card(Deck.deal())
+    Players_Hand.add_card(Deck.deal())
+
+    Delear_Hand = Hand.Hand()
+    Delears_Hand.add_card(Deck.deal())
+    Delears_Hand.add_card(Deck.deal())
+        
+    Player_Chips = Chips.Chips()
+    take_bet(Player_Chips)
+    
+    # Prompt the Player for their bet
+
+    
+    # Show cards (but keep one dealer card hidden)
+
+    
+    while playing:  # recall this variable from our hit_or_stand function
+        
+        # Prompt for Player to Hit or Stand
+        
+        
+        # Show cards (but keep one dealer card hidden)
+ 
+        
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+        
+
+            break
+
+    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+    
+    
+        # Show all cards
+    
+        # Run different winning scenarios
+        
+    
+    # Inform Player of their chips total 
+    
+    # Ask to play again
+
+        break
--- Modules/Chips.py
+++ Modules/Chips.py
@@ -12,24 +12,7 @@ class Chips:
     def win_bet(self):
         self.total += (self.bet*2)
         
-    def lose_bet(self):
+    def loose_bet(self):
         self.total -= self.bet
         
-#*****************************************************************************************************
-#      This function ask for bet to the player
-#*****************************************************************************************************
-
-def take_bet(Chips):
-
-    while True:
-            try:
-                Chips.bet = int(input("Please, Enter your bet : "))
-            except:
-                print("Enter interger numbers only.")
-            else:
-                if Chips.bet > Chips.total:
-                    Print(f"Bet must be Less then availble balance {Chips.total}. ")
-                    continue
-                else:
-                    break
-            
+`
